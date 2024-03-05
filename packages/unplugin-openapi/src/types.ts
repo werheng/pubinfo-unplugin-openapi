e886168f0c4e82a6c042d0158508efbdca0ec837
Werheng <vhang97@163.com>
2024-03-05 15:46:11 +0800

    fix(types): 删除多余的类型

--- packages/unplugin-openapi/src/types.ts
+++ packages/unplugin-openapi/src/types.ts
@@ -1,19 +1,4 @@
-import type { Import } from 'unimport'
-import type { FilterPattern } from '@rollup/pluginutils'
-
-export interface ImportExtended extends Import {
-  sideEffects?: SideEffectsInfo
-  __source?: 'dir' | 'resolver'
-}
-
-export interface ResolverResult {
-  as?: string
-  name?: string
-  from: string
-}
-
 export type Arrayable<T> = T | Array<T>
-export type SideEffectsInfo = Arrayable<ResolverResult | string> | undefined
 
 export interface CacheOptions {
   root?: string
@@ -63,43 +48,4 @@ export interface Options {
    * 批量配置
    */
   batch?: Array<Pick<Options, 'imports' | 'input' | 'output' | 'force' | 'manual'>>
-
-  /**
-   * 生成的.d.ts类型文件的路径。
-   * 在本地有安装 `typescript` 时默认使用。
-   * 设置 `false` 禁用。
-   *
-   * @default './openapi.d.ts'
-   */
-  dts?: string | boolean
-
-  /**
-   * Auto import inside Vue templates
-   *
-   * @see https://github.com/unjs/unimport/pull/15
-   * @see https://github.com/unjs/unimport/pull/72
-   * @default false
-   */
-  vueTemplate?: boolean
-
-  /**
-   * Set default export alias by file name
-   *
-   * @default false
-   */
-  defaultExportByFilename?: boolean
-
-  /**
-   * Rules to include transforming target.
-   *
-   * @default [/\.[jt]sx?$/, /\.vue\??/]
-   */
-  include?: FilterPattern
-
-  /**
-   * Rules to exclude transforming target.
-   *
-   * @default [/node_modules/, /\.git/]
-   */
-  exclude?: FilterPattern
 }
